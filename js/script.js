5b0cd6f88f4dedb0440bbdbeeb6dc1103a6e9fbd
Khushi1255 <khushii1255@gmail.com>
2024-06-15 09:25:17 +0530

    First Commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* =================typing animation =============*/
+// source skip: avoid-using-var
+var typed =new Typed(".typing",{
+    strings:["","Web Developer", "Web Designer", "Coding Enthusiast" , "Data Analyst"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
